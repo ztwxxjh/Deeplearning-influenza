f568983f5f16cac8ae5be6d09e65428a00455f67
ztwxxjh <371459795@qq.com>
2018-09-23 20:47:31 +0800

    minibatchs_sigmoid

--- Deeplearning/test.py
+++ Deeplearning/test.py
@@ -1,9 +1,7 @@
 import numpy as np
-import h5py
 import tensorflow as tf
 from tensorflow.python.framework import ops
 import tf_units
-import time
 # 读取数据
 X_train_orig, Y_train_orig, X_test_orig, Y_test_orig = tf_units.load_dataset()
 
@@ -12,7 +10,8 @@ X_test = X_test_orig / 410
 Y_train = Y_train_orig
 Y_test = Y_test_orig
 
-def create_placeholders(n_x,n_y):
+
+def create_placeholders(n_x, n_y):
     X = tf.placeholder(tf.float32, [n_x, None], name="X")
     Y = tf.placeholder(tf.float32, [n_y, None], name="Y")
     return X, Y
@@ -21,7 +20,7 @@ def create_placeholders(n_x,n_y):
 def initialize_parameters():
     """
     初始化神经网络的参数，参数的维度如下：
-        W1 : [25, 12288]
+        W1 : [25, 239]
         b1 : [25, 1]
         W2 : [12, 25]
         b2 : [12, 1]
@@ -38,10 +37,10 @@ def initialize_parameters():
 
     W1 = tf.get_variable("W1",  [25, 329], initializer=tf.contrib.layers.xavier_initializer(seed=1))
     b1 = tf.get_variable("b1", [25, 1], initializer=tf.zeros_initializer())
-    W2 = tf.get_variable("W2", [12, 25], initializer = tf.contrib.layers.xavier_initializer(seed=1))
-    b2 = tf.get_variable("b2", [12, 1], initializer = tf.zeros_initializer())
-    W3 = tf.get_variable("W3", [6, 12], initializer = tf.contrib.layers.xavier_initializer(seed=1))
-    b3 = tf.get_variable("b3", [6, 1], initializer = tf.zeros_initializer())
+    W2 = tf.get_variable("W2", [12, 25], initializer=tf.contrib.layers.xavier_initializer(seed=1))
+    b2 = tf.get_variable("b2", [12, 1], initializer=tf.zeros_initializer())
+    W3 = tf.get_variable("W3", [1, 12], initializer=tf.contrib.layers.xavier_initializer(seed=1))
+    b3 = tf.get_variable("b3", [1, 1], initializer=tf.zeros_initializer())
 
     parameters = {"W1": W1,
                   "b1": b1,
@@ -81,4 +80,106 @@ def forward_propagation(X, parameters):
     Z3 = tf.add(tf.matmul(W3, A2), b3)     # Z3 = np.dot(W3,Z2) + b3
     return Z3
 
+
+def compute_cost(Z3, Y):
+    logits = tf.transpose(Z3)
+    labels = tf.transpose(Y)
+    cost = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=logits, labels=labels))
+    return cost
+
+
+def model(X_train, Y_train, X_test, Y_test, learning_rate=0.0001, num_epochs=5000, minibatch_size=32, print_cost=True):
+    """
+    实现一个三层的TensorFlow神经网络：LINEAR->RELU->LINEAR->RELU->LINEAR->SOFTMAX
+
+    参数：
+        X_train - 训练集，维度为（输入大小（输入节点数量） = 12288, 样本数量 = 1080）
+        Y_train - 训练集分类数量，维度为（输出大小(输出节点数量) = 6, 样本数量 = 1080）
+        X_test - 测试集，维度为（输入大小（输入节点数量） = 12288, 样本数量 = 120）
+        Y_test - 测试集分类数量，维度为（输出大小(输出节点数量) = 6, 样本数量 = 120）
+        learning_rate - 学习速率
+        num_epochs - 整个训练集的遍历次数
+        mini_batch_size - 每个小批量数据集的大小
+        print_cost - 是否打印成本，每100代打印一次
+        is_plot - 是否绘制曲线图
+
+    返回：
+        parameters - 学习后的参数
+
+    """
+    ops.reset_default_graph()                #能够重新运行模型而不覆盖tf变量
+    tf.set_random_seed(1)
+    seed = 3
+    (n_x, m) = X_train.shape               #获取输入节点数量和样本数
+    n_y = Y_train.shape[0]                   #获取输出节点数量
+    costs = []                               #成本集
+
+    #给X和Y创建placeholder
+    X, Y = create_placeholders(n_x, n_y)
+
+    #初始化参数
+    parameters = initialize_parameters()
+
+    #前向传播
+    Z3 = forward_propagation(X, parameters)
+
+    #计算成本
+    cost = compute_cost(Z3, Y)
+
+    #反向传播，使用Adam优化
+    optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(cost)
+
+    #初始化所有的变量
+    init = tf.global_variables_initializer()
+
+    #开始会话并计算
+    with tf.Session() as sess:
+        #初始化
+        sess.run(init)
+
+        #正常训练的循环
+        for epoch in range(num_epochs):
+
+            epoch_cost = 0  #每代的成本
+            num_minibatches = int(m / minibatch_size)    #minibatch的总数量
+            seed = seed + 1
+            minibatches = tf_units.random_mini_batches(X_train, Y_train, minibatch_size, seed)
+
+            for minibatch in minibatches:
+
+                #选择一个minibatch
+                (minibatch_X, minibatch_Y) = minibatch
+
+                #数据已经准备好了，开始运行session
+                _, minibatch_cost = sess.run([optimizer, cost], feed_dict={X:minibatch_X, Y:minibatch_Y})
+
+                #计算这个minibatch在这一代中所占的误差
+                epoch_cost = epoch_cost + minibatch_cost / num_minibatches
+
+            #记录并打印成本
+            ## 记录成本
+            if epoch % 5 == 0:
+                costs.append(epoch_cost)
+                #是否打印：
+                if print_cost and epoch % 100 == 0:
+                        print("epoch = " + str(epoch) + "    epoch_cost = " + str(epoch_cost))
+
+        #保存学习后的参数
+        parameters = sess.run(parameters)
+        print("参数已经保存到session。")
+
+        #计算当前的预测结果
+        correct_prediction = tf.equal(sigmoid(Z3), Y)
+
+        #计算准确率
+        accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float"))
+
+        print("训练集的准确率：", accuracy.eval({X: X_train, Y: Y_train}))
+        print("测试集的准确率:", accuracy.eval({X: X_test, Y: Y_test}))
+
+        return parameters
+
+
+
+parameters = model(X_train, Y_train, X_test, Y_test)
 
